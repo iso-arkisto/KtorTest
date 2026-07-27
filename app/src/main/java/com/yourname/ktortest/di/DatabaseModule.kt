cb235e0f04ac0ecd941cd85b6d9b8cf0beb45894
Andrey <Fian.123@yandex.ru>
2026-07-27 18:56:38 +0300

    feat: add MyApp and DatabaseModule

---
+++ app/src/main/java/com/yourname/ktortest/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.yourname.ktortest.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.yourname.ktortest.data.local.LanguageDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+        context = context,
+        klass = LanguageDatabase::class.java,
+            name = "language_database.db"
+    ).build()
+}
