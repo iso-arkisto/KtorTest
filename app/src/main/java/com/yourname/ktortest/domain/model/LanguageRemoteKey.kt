f2757a031174a178808d5402ce015edf444eeccc
Andrey <Fian.123@yandex.ru>
2026-08-14 17:28:17 +0300

    fix: add missing parameter to LanguageRemoteKey

--- app/src/main/java/com/yourname/ktortest/domain/model/LanguageRemoteKey.kt
+++ app/src/main/java/com/yourname/ktortest/domain/model/LanguageRemoteKey.kt
@@ -3,7 +3,7 @@ package com.yourname.ktortest.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "languageremotekey")
 data class LanguageRemoteKey(
     @PrimaryKey(autoGenerate = false) val id: Int,
     val prevPage: Int?,
