bbcf7eba7ce0be98d09011fddb451449fb7d7400
Andrey <Fian.123@yandex.ru>
2026-08-14 17:34:51 +0300

    feat: add new variables for onboarding and preferences to Constants object

--- app/src/main/java/com/yourname/ktortest/utils/Constants.kt
+++ app/src/main/java/com/yourname/ktortest/utils/Constants.kt
@@ -2,4 +2,8 @@ package com.yourname.ktortest.utils
 
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "id"
+    const val ONBOARDING_PAGE_COUNT = 3
+    const val LAST_ONBOARDING_PAGE = 3
+    const val PREFERENCES_NAME = "ktortest"
+    const val PREFERENCES_KEY = "onboarding_completed"
 }
