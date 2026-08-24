acdecabe141f640f74097ff7f51b035516cf7dba
Andrey <Fian.123@yandex.ru>
2026-08-24 18:19:11 +0300

    feat(utils): add variable BASE_URL in Constants

--- app/src/main/java/com/yourname/ktortest/utils/Constants.kt
+++ app/src/main/java/com/yourname/ktortest/utils/Constants.kt
@@ -7,4 +7,5 @@ object Constants {
     const val PREFERENCES_NAME = "ktortest"
     const val PREFERENCES_KEY = "onboarding_completed"
     const val SPLASH_DELAY = 2000L
+    const val BASE_URL = "http://10.0.2.2:8080"
 }
