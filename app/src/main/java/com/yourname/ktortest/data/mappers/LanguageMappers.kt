21d64fc004ceac8ced6de7d178399cebedb3db4a
Andrey <Fian.123@yandex.ru>
2026-09-01 11:53:53 +0300

    feat(data): add new mapper function and relocate file LanguageMappers

--- app/src/main/java/com/yourname/ktortest/data/mappers/LanguageMappers.kt
+++ app/src/main/java/com/yourname/ktortest/data/mappers/LanguageMappers.kt
@@ -1,10 +1,21 @@
-package com.yourname.ktortest.data.paging_source
+package com.yourname.ktortest.data.mappers
 
 import com.yourname.ktortest.data.local.LanguageEntity
 import com.yourname.ktortest.domain.model.ProgrammingLanguage
 
 fun ProgrammingLanguage.toEntity(): LanguageEntity {
     return LanguageEntity(
+        id = id,
+        image = image,
+        shortName = shortName,
+        creator = creator,
+        inceptionYear = inceptionYear,
+        name = name
+    )
+}
+
+fun LanguageEntity.toDomain(): ProgrammingLanguage {
+    return ProgrammingLanguage(
         id = id,
         image = image,
         shortName = shortName,
