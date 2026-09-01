d8967daa6c52ec65c977636e076bb502e08a02e6
Andrey <Fian.123@yandex.ru>
2026-09-01 12:38:40 +0300

    feat(data): inject RemoteDataSource and add function getAllLanguages in Repository

--- app/src/main/java/com/yourname/ktortest/data/repository/Repository.kt
+++ app/src/main/java/com/yourname/ktortest/data/repository/Repository.kt
@@ -1,15 +1,25 @@
 package com.yourname.ktortest.data.repository
 
+import androidx.paging.PagingData
+import com.yourname.ktortest.domain.model.ProgrammingLanguage
 import com.yourname.ktortest.domain.repository.DatastoreOperations
+import com.yourname.ktortest.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class Repository @Inject constructor(val datastoreOperations: DatastoreOperations) {
+class Repository @Inject constructor(
+    val datastoreOperations: DatastoreOperations,
+    val remoteDataSource: RemoteDataSource
+) {
     suspend fun saveOnboardingState(isCompleted: Boolean) {
         datastoreOperations.saveOnboardingState(isCompleted)
     }
 
     fun readOnboardingState(): Flow<Boolean> {
         return datastoreOperations.readOnboardingState()
+    }
+
+    fun getAllLanguages(): Flow<PagingData<ProgrammingLanguage>> {
+        return remoteDataSource.getAllLanguages()
     }
 }
