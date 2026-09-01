6183f67789c24bc5d0116b994d69369e6501a8cd
Andrey <Fian.123@yandex.ru>
2026-09-01 12:39:52 +0300

    feat(di): add function provideRemoteDataSource in RepositoryModule

--- app/src/main/java/com/yourname/ktortest/di/RepositoryModule.kt
+++ app/src/main/java/com/yourname/ktortest/di/RepositoryModule.kt
@@ -1,9 +1,14 @@
 package com.yourname.ktortest.di
 
+import com.yourname.ktortest.data.local.LanguageDatabase
+import com.yourname.ktortest.data.remote.KtorApi
 import com.yourname.ktortest.data.repository.DatastoreOperationsImpl
+import com.yourname.ktortest.data.repository.RemoteDataSourceImpl
 import com.yourname.ktortest.domain.repository.DatastoreOperations
+import com.yourname.ktortest.domain.repository.RemoteDataSource
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -14,4 +19,13 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindDatastoreOperations(datastoreOperationsImpl: DatastoreOperationsImpl): DatastoreOperations
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(
+        ktorApi: KtorApi,
+        database: LanguageDatabase
+    ): RemoteDataSource {
+        return RemoteDataSourceImpl(ktorApi, database)
+    }
 }
