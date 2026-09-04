99cfaf40749ea4856ac2d4b9b8cee727c2a10298
Andrey <Fian.123@yandex.ru>
2026-09-04 18:44:38 +0300

    feat(domain): add getAllLanguagesUseCase parameter to UseCases

--- app/src/main/java/com/yourname/ktortest/domain/usecase/UseCases.kt
+++ app/src/main/java/com/yourname/ktortest/domain/usecase/UseCases.kt
@@ -1,9 +1,11 @@
 package com.yourname.ktortest.domain.usecase
 
+import com.yourname.ktortest.domain.usecase.get_all_languages.GetAllLanguagesUseCase
 import com.yourname.ktortest.domain.usecase.read_onboarding.ReadOnboardingUseCase
 import com.yourname.ktortest.domain.usecase.save_onboarding.SaveOnboardingUseCase
 
 data class UseCases(
     val readOnboardingUseCase: ReadOnboardingUseCase,
-    val saveOnboardingUseCase: SaveOnboardingUseCase
+    val saveOnboardingUseCase: SaveOnboardingUseCase,
+    val getAllLanguagesUseCase: GetAllLanguagesUseCase
 )
