834be1ff0eaed49bc634adcd3f492a9593eedcc7
Andrey <Fian.123@yandex.ru>
2026-09-04 18:45:19 +0300

    feat(di): add function provideUseCases to RepositoryModule

--- app/src/main/java/com/yourname/ktortest/di/RepositoryModule.kt
+++ app/src/main/java/com/yourname/ktortest/di/RepositoryModule.kt
@@ -4,8 +4,13 @@ import com.yourname.ktortest.data.local.LanguageDatabase
 import com.yourname.ktortest.data.remote.KtorApi
 import com.yourname.ktortest.data.repository.DatastoreOperationsImpl
 import com.yourname.ktortest.data.repository.RemoteDataSourceImpl
+import com.yourname.ktortest.data.repository.Repository
 import com.yourname.ktortest.domain.repository.DatastoreOperations
 import com.yourname.ktortest.domain.repository.RemoteDataSource
+import com.yourname.ktortest.domain.usecase.UseCases
+import com.yourname.ktortest.domain.usecase.get_all_languages.GetAllLanguagesUseCase
+import com.yourname.ktortest.domain.usecase.read_onboarding.ReadOnboardingUseCase
+import com.yourname.ktortest.domain.usecase.save_onboarding.SaveOnboardingUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -27,5 +32,17 @@ abstract class RepositoryModule {
         database: LanguageDatabase
     ): RemoteDataSource {
         return RemoteDataSourceImpl(ktorApi, database)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUseCases(
+        repository: Repository
+    ): UseCases {
+        return UseCases(
+            readOnboardingUseCase = ReadOnboardingUseCase(repository),
+            saveOnboardingUseCase = SaveOnboardingUseCase(repository),
+            getAllLanguagesUseCase = GetAllLanguagesUseCase(repository)
+        )
     }
 }
