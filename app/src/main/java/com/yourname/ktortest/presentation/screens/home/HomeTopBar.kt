f25c0154e84fea967ad831c704b79cca99cdd902
Andrey <Fian.123@yandex.ru>
2026-09-12 13:48:31 +0300

    feat(presentation): create HomeTopBar ui component

---
+++ app/src/main/java/com/yourname/ktortest/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,65 @@
+package com.yourname.ktortest.presentation.screens.home
+
+import android.content.res.Configuration
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.yourname.ktortest.ui.theme.KtorTestTheme
+import com.yourname.ktortest.ui.theme.topAppBarBackgroundColor
+import com.yourname.ktortest.ui.theme.topAppBarContentColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = topAppBarBackgroundColor(),
+        ),
+        title = {
+            Text(
+                text = "Explore",
+                color = topAppBarContentColor()
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = onSearchClicked
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark Theme"
+)
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "Light Theme"
+)
+fun HomeTopBarDarkPreview() {
+    KtorTestTheme {
+        HomeTopBar(
+            onSearchClicked = {}
+        )
+    }
+}
