992c81f3be1e63ab40c17e199e5edc73d2846780
Andrey <Fian.123@yandex.ru>
2026-09-15 19:41:05 +0300

    fix(presentation): change modifier from height to size for circles in ShimmerEffect

--- app/src/main/java/com/yourname/ktortest/presentation/components/ShimmerEffect.kt
+++ app/src/main/java/com/yourname/ktortest/presentation/components/ShimmerEffect.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -106,7 +107,7 @@ fun ShimmerItem(
                     Surface(
                         modifier = Modifier
                             .alpha(alpha)
-                            .height(20.dp),
+                            .size(20.dp),
                         color = shimmerComponentItemColor(),
                         shape = RoundedCornerShape(SMALL_CORNER)
                     ) { }
