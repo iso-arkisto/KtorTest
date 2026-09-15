9f1c51568c0152a156d4ac37678cc120326c95c0
Andrey <Fian.123@yandex.ru>
2026-09-15 19:42:50 +0300

    fix(utils): add missing slash to BASE_URL constant

--- app/src/main/java/com/yourname/ktortest/utils/Constants.kt
+++ app/src/main/java/com/yourname/ktortest/utils/Constants.kt
@@ -7,6 +7,6 @@ object Constants {
     const val PREFERENCES_NAME = "ktortest"
     const val PREFERENCES_KEY = "onboarding_completed"
     const val SPLASH_DELAY = 2000L
-    const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "http://10.0.2.2:8080/"
     const val ITEMS_PER_PAGE = 3
 }
